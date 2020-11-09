0f92d7082c2d8c8c9d8b3a75851bd2bcae987a13
Sayema Islam <sayema.islam@gmail.com>
2020-11-09 16:32:29 -0500

    Started working on java code based on professor's ACID demo

--- project_code/src/team4project/PostgreSQLT4ACID.java
+++ project_code/src/team4project/PostgreSQLT4ACID.java
@@ -1,9 +1,47 @@
 package team4project;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PostgreSQLT4ACID {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+		
+		//Load PostgreSQL driver
+		Class.forName("org.postgresql.Driver");
+		
+		//Connect to the default database with credentials
+		//Enter your own machine's port # for postgre after localhost
+		String url = "jdbc:postgresql://localhost:5432/";
+		//Enter your own machine's root here
+		String root = "postgre";
+		//Enter your own postgre password here
+		String password = "password";
+				
+		Connection connect = DriverManager.getConnection(url, root, password);
+		
+		//For Atomicity
+		connect.setAutoCommit(false);
+		
+		//For Isolation
+		connect.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+		
+		Statement query = null;
+		try {
+			
+			//Create statement object for sending SQL statements to the database
+			query = connect.createStatement();
+			
+			//
+			
+		} catch (SQLException e){
+			System.out.println("An exception was thrown");
+		}
+		
 
 	}
 
